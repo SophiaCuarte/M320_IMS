f701099491ee7bb461175a8777bd045a2ad41ae5
Sophia Cuarte <Sophia.Cuarte@ksb-sg.ch>
2023-08-27 20:13:36 +0200

    Aufgabe 1 erfüllt

---
+++ M320_IMS/MyMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class MyMath
+{
+    public static int Calc_ggT(int a, int b)
+    {
+        while (b != 0) (b, a) = (a % b, b);
+        return a;
+    }
+
+    public static int Calc_kgV(int a, int b) => (a * b) / Calc_ggT(a, b);
+
+    public static int ReadInt(string prompt)
+    {
+        int result;
+        do Console.Write(prompt);
+        while (!int.TryParse(Console.ReadLine(), out result) || result <= 0);
+        return result;
+    }
+
+    public static void ShowResult(string operation, int a, int b, int result) =>
+        Console.WriteLine($"{operation} von {a} und {b} ist {result}");
+
+    public static int Calc_ggT_r(int a, int b) => b == 0 ? a : Calc_ggT_r(b, a % b);
+
+    public static double CalcArithmeticMean(int[] numbers) => (double)numbers.Sum() / numbers.Length;
+
+    public static int FindSmallestValue(int[] numbers) => numbers.Min();
+
+    public static int FindLargestValue(int[] numbers) => numbers.Max();
+
+    public static void Swap(ref int a, ref int b) => (a, b) = (b, a);
+
+    public static void ReverseArray(int[] array)
+    {
+        for (int left = 0, right = array.Length - 1; left < right; left++, right--)
+            Swap(ref array[left], ref array[right]);
+    }
+}
--- M320_IMS/Program.cs
+++ M320_IMS/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace M320_IMS
+﻿class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        
+        while (true)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1. ggT berechnen");
+            Console.WriteLine("2. kgV berechnen");
+            Console.WriteLine("3. Array-Funktionen");
+            Console.WriteLine("4. Beenden");
+            Console.Write("Option wählen: ");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            if (option == 4)
+                break;
+
+            if (option < 1 || option > 3)
+            {
+                Console.WriteLine("Ungültige Option!");
+                continue;
+            }
+
+            int a = MyMath.ReadInt("Geben Sie die erste Zahl ein: ");
+            int b = MyMath.ReadInt("Geben Sie die zweite Zahl ein: ");
+
+
+            if (option == 1)
+                MyMath.ShowResult("ggT", a, b, MyMath.Calc_ggT(a, b));
+            else if (option == 2)
+                MyMath.ShowResult("kgV", a, b, MyMath.Calc_kgV(a, b));
+            else if (option == 3)
+            {
+                int arraySize = MyMath.ReadInt("Geben Sie die Größe des Arrays ein: ");
+                int[] numbers = new int[arraySize];
+                for (int i = 0; i < arraySize; i++)
+                    numbers[i] = MyMath.ReadInt($"Geben Sie die Zahl an Index {i} ein: ");
+
+                Console.WriteLine($"Arithmetischer Mittelwert: {MyMath.CalcArithmeticMean(numbers)}");
+                Console.WriteLine($"Kleinster Wert: {MyMath.FindSmallestValue(numbers)}");
+                Console.WriteLine($"Größter Wert: {MyMath.FindLargestValue(numbers)}");
+
+                MyMath.ReverseArray(numbers);
+                Console.WriteLine("Umgekehrtes Array: " + string.Join(" ", numbers));
+            }
+
+            Console.WriteLine();
         }
     }
 }
