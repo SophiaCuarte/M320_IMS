5a8d2ea52d8dccbcf1d703f0e90c185929f3d346
Sophia Cuarte <Sophia.Cuarte@ksb-sg.ch>
2023-09-03 21:58:15 +0200

    Taschenrechner

---
+++ Taschenrechner/Kalkulation.cs
@@ -0,0 +1,73 @@
+﻿class Calculator
+{
+    private int lastResult = 0; // Interner Zustand zur Speicherung des letzten Ergebnisses
+
+    public int Add(params int[] operands)
+    {
+        int sum = lastResult;
+
+        foreach (int operand in operands)
+        {
+            sum += operand;
+        }
+
+        lastResult = sum;
+        return lastResult;
+    }
+
+    public int Subtract(params int[] operands)
+    {
+        if (operands.Length < 1)
+        {
+            Console.WriteLine("Es wurden nicht genügend Argumente angegeben.");
+            return lastResult;
+        }
+
+        int result = operands[0];
+
+        for (int i = 1; i < operands.Length; i++)
+        {
+            result -= operands[i];
+        }
+
+        lastResult = result;
+        return lastResult;
+    }
+
+    public int Multiply(params int[] operands)
+    {
+        int product = 1;
+
+        foreach (int operand in operands)
+        {
+            product *= operand;
+        }
+
+        lastResult = product;
+        return lastResult;
+    }
+
+    public int Divide(params int[] operands)
+    {
+        if (operands.Length < 1)
+        {
+            Console.WriteLine("Es wurden nicht genügend Argumente angegeben.");
+            return lastResult;
+        }
+
+        int result = operands[0];
+
+        for (int i = 1; i < operands.Length; i++)
+        {
+            if (operands[i] == 0)
+            {
+                Console.WriteLine("Division durch Null ist nicht erlaubt.");
+                return lastResult;
+            }
+            result /= operands[i];
+        }
+
+        lastResult = result;
+        return lastResult;
+    }
+}
---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+
+        Console.WriteLine("Einfacher Taschenrechner");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraktion");
+        Console.WriteLine("3. Multiplikation");
+        Console.WriteLine("4. Division");
+        Console.WriteLine("5. Beenden");
+
+        char option;
+        do
+        {
+            Console.Write("Wählen Sie eine Option (1/2/3/4/5): ");
+            option = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            if (option >= '1' && option <= '4')
+            {
+                Console.Write("Geben Sie die erste Zahl ein: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Geben Sie die zweite Zahl ein: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int result = 0;
+
+                switch (option)
+                {
+                    case '1':
+                        result = calculator.Add(num1, num2);
+                        break;
+                    case '2':
+                        result = calculator.Subtract(num1, num2);
+                        break;
+                    case '3':
+                        result = calculator.Multiply(num1, num2);
+                        break;
+                    case '4':
+                        result = calculator.Divide(num1, num2);
+                        break;
+                }
+
+                Console.WriteLine("Ergebnis: " + result);
+            }
+            else if (option == '5')
+            {
+                Console.WriteLine("Das Programm wird beendet.");
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Option. Bitte wählen Sie erneut.");
+            }
+
+        } while (option != '5');
+    }
+}
