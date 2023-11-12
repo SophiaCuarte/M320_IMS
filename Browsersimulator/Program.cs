a12fa46392ae8910a314c80af682273d762e8263
sophi <sophi@DESKTOP-GI29BKE>
2023-11-12 13:25:29 +0100

    browsersimulator fertig

--- Browsersimulator/Program.cs
+++ Browsersimulator/Program.cs
@@ -1,11 +1,30 @@
-﻿namespace Browsersimulator
+﻿using System;
+
+namespace Browsersimulator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Simulator simulator = new Simulator();
-            simulator.Run();
+            StringStack stack = new StringStack(5);
+
+            stack.Push("Value1");
+            stack.Push("Value2");
+            stack.Push("Value3");
+
+            string topValue = stack.Peek();
+            Console.WriteLine($"Top value on the stack: {topValue}");
+
+            string poppedValue = stack.Pop();
+            Console.WriteLine($"Popped value from the stack: {poppedValue}");
+
+            stack.Swap();
+
+            Console.WriteLine("Remaining values in the stack:");
+            while (!stack.IsEmpty)
+            {
+                Console.WriteLine(stack.Pop());
+            }
         }
     }
 }
--- Browsersimulator/StringStack.cs
+++ Browsersimulator/StringStack.cs
@@ -1,27 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Browsersimulator
 {
     internal class StringStack
     {
-        string[] values;
-        int index;
-        public StringStack()
+        private string[] values;
+        private int index;
+
+        public StringStack(int size)
         {
-            values = new string[10];
+            values = new string[size];
+            index = 0;
         }
 
         public void Push(string value)
         {
-            this.values[index++] = value;
+            if (IsFull)
+            {
+                Console.WriteLine("Stack is full. Cannot push more elements.");
+                return;
+            }
+
+            values[index++] = value;
         }
+
         public string Pop()
         {
-           return this.values[index--];
+            if (IsEmpty)
+            {
+                Console.WriteLine("Stack is empty. Cannot pop elements.");
+                return null;
+            }
+
+            return values[--index];
         }
+
+        public string Peek()
+        {
+            if (IsEmpty)
+            {
+                Console.WriteLine("Stack is empty. Cannot peek elements.");
+                return null;
+            }
+
+            return values[index - 1];
+        }
+
+        public void Swap()
+        {
+            if (index < 2)
+            {
+                Console.WriteLine("Stack has less than two elements. Cannot swap.");
+                return;
+            }
+
+            string temp = values[index - 1];
+            values[index - 1] = values[index - 2];
+            values[index - 2] = temp;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(values, 0, values.Length);
+            index = 0;
+        }
+
+        public bool IsEmpty => index == 0;
+
+        public int Count => index;
+
+        public bool IsFull => index == values.Length;
     }
 }
