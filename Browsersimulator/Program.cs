dcdc41f5fcb48e3bc8c9875034ed50a636ce2747
Sophia Cuarte <Sophia.Cuarte@ksb-sg.ch>
2023-11-13 08:43:05 +0100

    input

--- Browsersimulator/Program.cs
+++ Browsersimulator/Program.cs
@@ -8,17 +8,16 @@ namespace Browsersimulator
         {
             StringStack stack = new StringStack(5);
 
-            stack.Push("Value1");
-            stack.Push("Value2");
-            stack.Push("Value3");
+            while (true)
+            {
+                Console.WriteLine("Enter a URL or 'e' to exit.");
+                string input = Console.ReadLine();
 
-            string topValue = stack.Peek();
-            Console.WriteLine($"Top value on the stack: {topValue}");
+                if (input.ToLower() == "e")
+                    break;
 
-            string poppedValue = stack.Pop();
-            Console.WriteLine($"Popped value from the stack: {poppedValue}");
-
-            stack.Swap();
+                stack.Push(input);
+            }
 
             Console.WriteLine("Remaining values in the stack:");
             while (!stack.IsEmpty)
